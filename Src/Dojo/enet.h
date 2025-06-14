d493501f9cc1ffb7e19425dff789143768adf6d6
Enrique Santos <blueminder@users.noreply.github.com>
2025-06-15 01:17:49 +0300

    enet: remove extraneous clock_gettime, windows

--- Src/Dojo/enet.h
+++ Src/Dojo/enet.h
@@ -4902,44 +4902,6 @@ extern "C" {
             t.QuadPart |= f.dwLowDateTime;
             return (t);
         }
-        int clock_gettime(int X, struct timespec *tv) {
-            (void)X;
-            LARGE_INTEGER t;
-            FILETIME f;
-            double microseconds;
-            static LARGE_INTEGER offset;
-            static double frequencyToMicroseconds;
-            static int initialized = 0;
-            static BOOL usePerformanceCounter = 0;
-
-            if (!initialized) {
-                LARGE_INTEGER performanceFrequency;
-                initialized = 1;
-                usePerformanceCounter = QueryPerformanceFrequency(&performanceFrequency);
-                if (usePerformanceCounter) {
-                    QueryPerformanceCounter(&offset);
-                    frequencyToMicroseconds = (double)performanceFrequency.QuadPart / 1000000.;
-                } else {
-                    offset = getFILETIMEoffset();
-                    frequencyToMicroseconds = 10.;
-                }
-            }
-            if (usePerformanceCounter) {
-                QueryPerformanceCounter(&t);
-            } else {
-                GetSystemTimeAsFileTime(&f);
-                t.QuadPart = f.dwHighDateTime;
-                t.QuadPart <<= 32;
-                t.QuadPart |= f.dwLowDateTime;
-            }
-
-            t.QuadPart -= offset.QuadPart;
-            microseconds = (double)t.QuadPart / frequencyToMicroseconds;
-            t.QuadPart = (LONGLONG)microseconds;
-            tv->tv_sec = (long)(t.QuadPart / 1000000);
-            tv->tv_nsec = t.QuadPart % 1000000 * 1000;
-            return (0);
-        }
     #elif __APPLE__ && __MAC_OS_X_VERSION_MIN_REQUIRED < 101200
         #define CLOCK_MONOTONIC 0
 
